541c50f74339b2f72d9c8bde2f375f7ee5726fd7
Saleh Furqan <iamsaleh.furqan@gmail.com>
2024-04-06 13:57:44 +0800

    components added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,94 @@
-import { useSession } from "next-auth/react";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import Link from 'next/link'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import { Icons } from './Icons'
+import NavItems from './NavItems'
+import { buttonVariants } from './ui/button'
+import Cart from './Cart'
+import { getServerSideUser } from '@/lib/payload-utils'
+import { cookies } from 'next/headers'
+import UserAccountNav from './UserAccountNav'
+import MobileNav from './MobileNav'
 
-function Navbar() {
-  const { data } = useSession();
-  const user = data?.user;
+const Navbar = async () => {
+  const nextCookies = cookies()
+  const { user } = await getServerSideUser(nextCookies)
+
   return (
-    <nav className="flex h-14 items-center border px-10 py-1">
-      <Link href="/">
-        <Image src="/logo.png" alt="logo" width={120} height={30} />
-      </Link>
-      {user && (
-        <Link href="/account" className="ml-auto">
-          <Avatar>
-            <AvatarImage src={user.image!} />
-            <AvatarFallback>{user.name}</AvatarFallback>
-          </Avatar>
-        </Link>
-      )}
-    </nav>
-  );
+    <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
+      <header className='relative bg-white'>
+        <MaxWidthWrapper>
+          <div className='border-b border-gray-200'>
+            <div className='flex h-16 items-center'>
+              <MobileNav />
+
+              <div className='ml-4 flex lg:ml-0'>
+                <Link href='/'>
+                  <Icons.logo className='h-10 w-10' />
+                </Link>
+              </div>
+
+              <div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
+                <NavItems />
+              </div>
+
+              <div className='ml-auto flex items-center'>
+                <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
+                  {user ? null : (
+                    <Link
+                      href='/sign-in'
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}>
+                      Sign in
+                    </Link>
+                  )}
+
+                  {user ? null : (
+                    <span
+                      className='h-6 w-px bg-gray-200'
+                      aria-hidden='true'
+                    />
+                  )}
+
+                  {user ? (
+                    <UserAccountNav user={user} />
+                  ) : (
+                    <Link
+                      href='/sign-up'
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}>
+                      Create account
+                    </Link>
+                  )}
+
+                  {user ? (
+                    <span
+                      className='h-6 w-px bg-gray-200'
+                      aria-hidden='true'
+                    />
+                  ) : null}
+
+                  {user ? null : (
+                    <div className='flex lg:ml-6'>
+                      <span
+                        className='h-6 w-px bg-gray-200'
+                        aria-hidden='true'
+                      />
+                    </div>
+                  )}
+
+                  <div className='ml-4 flow-root lg:ml-6'>
+                    <Cart />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
+    </div>
+  )
 }
 
-export default Navbar;
+export default Navbar
