1002d6ee9b5906a63014033ae57325c4fdf42f95
rejoanahmed <rejoanahmed8@gmail.com>
2024-04-08 14:57:00 +0800

    fix dialog state

---
+++ src/components/CreateStore.tsx
@@ -0,0 +1,94 @@
+import { Button } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import { Input } from "~/components/ui/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { storeSchema } from "~/utils/formSchema";
+import { type z } from "zod";
+import { api } from "~/utils/api";
+import { useState } from "react";
+
+export default function AccountPage() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const form = useForm<z.infer<typeof storeSchema>>({
+    resolver: zodResolver(storeSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+    },
+  });
+
+  const { mutate: CreateStore } = api.vendorStore.create.useMutation();
+  function onSubmit(values: z.infer<typeof storeSchema>) {
+    CreateStore(values);
+    setIsDialogOpen(false);
+  }
+  return (
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DialogTrigger asChild>
+        <Button variant="outline">Create a Store</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create STORE</DialogTitle>
+          <DialogDescription>
+            Create a store to start selling your products.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Store Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Fortress Hill" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public store name. It should be unique
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Store Description</FormLabel>
+                  <FormControl>
+                    <Input placeholder="We sell the best products" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Describe your store in 200 characters or less
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
