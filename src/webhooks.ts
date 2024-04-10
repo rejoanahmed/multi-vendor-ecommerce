3da5aad6ebaf2e66fc796075ddc9432411ed8514
YerzhanAbd <yerzhan.abd0@gmail.com>
2024-04-10 23:26:29 +0800

    stock logic

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -1,121 +1,117 @@
-import express from 'express'
-import { WebhookRequest } from './server'
-import { stripe } from './lib/stripe'
-import type Stripe from 'stripe'
-import { getPayloadClient } from './get-payload'
-import { Product } from './payload-types'
-import { Resend } from 'resend'
-import { ReceiptEmailHtml } from './components/emails/ReceiptEmail'
+import express from "express";
+import { WebhookRequest } from "./server";
+import { stripe } from "./lib/stripe";
+import type Stripe from "stripe";
+import { getPayloadClient } from "./get-payload";
+import { Product } from "./payload-types";
+import { Resend } from "resend";
+import { ReceiptEmailHtml } from "./components/emails/ReceiptEmail";
 
-const resend = new Resend('re_4d3jErur_JkpDLi4jdFDicw6LU6DJTq1J')
+const resend = new Resend("re_4d3jErur_JkpDLi4jdFDicw6LU6DJTq1J");
 
 export const stripeWebhookHandler = async (
-  req: express.Request,
-  res: express.Response
+	req: express.Request,
+	res: express.Response
 ) => {
-  const webhookRequest = req as any as WebhookRequest
-  const body = webhookRequest.rawBody
-  const signature = req.headers['stripe-signature'] || ''
+	const webhookRequest = req as any as WebhookRequest;
+	const body = webhookRequest.rawBody;
+	const signature = req.headers["stripe-signature"] || "";
 
-  let event
-  try {
-    event = stripe.webhooks.constructEvent(
-      body,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET || ''
-    )
-  } catch (err) {
-    return res
-      .status(400)
-      .send(
-        `Webhook Error: ${
-          err instanceof Error
-            ? err.message
-            : 'Unknown Error'
-        }`
-      )
-  }
+	let event;
+	try {
+		event = stripe.webhooks.constructEvent(
+			body,
+			signature,
+			process.env.STRIPE_WEBHOOK_SECRET || ""
+		);
+	} catch (err) {
+		return res
+			.status(400)
+			.send(
+				`Webhook Error: ${err instanceof Error ? err.message : "Unknown Error"}`
+			);
+	}
 
-  const session = event.data
-    .object as Stripe.Checkout.Session
+	const session = event.data.object as Stripe.Checkout.Session;
 
-  if (
-    !session?.metadata?.userId ||
-    !session?.metadata?.orderId
-  ) {
-    return res
-      .status(400)
-      .send(`Webhook Error: No user present in metadata`)
-  }
+	if (!session?.metadata?.userId || !session?.metadata?.orderId) {
+		return res.status(400).send(`Webhook Error: No user present in metadata`);
+	}
 
-  if (event.type === 'checkout.session.completed') {
-    const payload = await getPayloadClient()
+	if (event.type === "checkout.session.completed") {
+		const payload = await getPayloadClient();
 
-    const { docs: users } = await payload.find({
-      collection: 'users',
-      where: {
-        id: {
-          equals: session.metadata.userId,
-        },
-      },
-    })
+		const { docs: users } = await payload.find({
+			collection: "users",
+			where: {
+				id: {
+					equals: session.metadata.userId,
+				},
+			},
+		});
 
-    const [user] = users
+		const [user] = users;
 
-    if (!user)
-      return res
-        .status(404)
-        .json({ error: 'No such user exists.' })
+		if (!user) return res.status(404).json({ error: "No such user exists." });
 
-    const { docs: orders } = await payload.find({
-      collection: 'orders',
-      depth: 2,
-      where: {
-        id: {
-          equals: session.metadata.orderId,
-        },
-      },
-    })
+		const { docs: orders } = await payload.find({
+			collection: "orders",
+			depth: 2,
+			where: {
+				id: {
+					equals: session.metadata.orderId,
+				},
+			},
+		});
 
-    const [order] = orders
+		const [order] = orders;
 
-    if (!order)
-      return res
-        .status(404)
-        .json({ error: 'No such order exists.' })
+		if (!order) return res.status(404).json({ error: "No such order exists." });
 
-    await payload.update({
-      collection: 'orders',
-      data: {
-        _isPaid: true,
-      },
-      where: {
-        id: {
-          equals: session.metadata.orderId,
-        },
-      },
-    })
+		await payload.update({
+			collection: "orders",
+			data: {
+				_isPaid: true,
+			},
+			where: {
+				id: {
+					equals: session.metadata.orderId,
+				},
+			},
+		});
 
-    // send receipt
-    try {
-      const data = await resend.emails.send({
-        from: 'mutlistore <hello@rejoanahmed.me>',
-        to: [user.email],
-        subject:
-          'Thanks for your order! This is your receipt.',
-        html: ReceiptEmailHtml({
-          date: new Date(),
-          email: user.email,
-          orderId: session.metadata.orderId,
-          products: order.products as Product[],
-        }),
-      })
-      console.log('Receipt email sent:', data)
-      res.status(200).json({ data })
-    } catch (error) {
-      res.status(500).json({ error })
-    }
-  }
+		// decrement stock for each product
+		for (const product of order.products as Product[]) {
+			payload.update({
+				collection: "products",
+				data: { stock: product.stock - 1 },
+				where: {
+					id: {
+						equals: product.id,
+					},
+				},
+			});
+		}
 
-  return res.status(200).send()
-}
+		// send receipt
+		try {
+			const data = await resend.emails.send({
+				from: "mutlistore <hello@rejoanahmed.me>",
+				to: [user.email],
+				subject: "Thanks for your order! This is your receipt.",
+				html: ReceiptEmailHtml({
+					date: new Date(),
+					email: user.email,
+					orderId: session.metadata.orderId,
+					products: order.products as Product[],
+				}),
+			});
+			console.log("Receipt email sent:", data);
+			res.status(200).json({ data });
+		} catch (error) {
+			res.status(500).json({ error });
+		}
+	}
+
+	return res.status(200).send();
+};
