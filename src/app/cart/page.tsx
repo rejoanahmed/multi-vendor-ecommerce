a056738484b4b99599275edddf0f5dc733fe28c9
YerzhanAbd <yerzhan.abd0@gmail.com>
2024-04-11 01:20:49 +0800

    empty cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -5,7 +5,7 @@ import { PRODUCT_CATEGORIES } from "@/config";
 import { useCart } from "@/hooks/use-cart";
 import { cn, formatPrice } from "@/lib/utils";
 import { trpc } from "@/trpc/client";
-import { Check, Loader2, X } from "lucide-react";
+import { Check, Loader2, ShoppingBasket, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -55,17 +55,8 @@ const Page = () => {
 
 						{isMounted && items.length === 0 ? (
 							<div className="flex h-full flex-col items-center justify-center space-y-1">
-								<div
-									aria-hidden="true"
-									className="relative mb-4 h-40 w-40 text-muted-foreground"
-								>
-									<Image
-										src="/hippo-empty-cart.png"
-										fill
-										loading="eager"
-										alt="empty shopping cart hippo"
-									/>
-								</div>
+								<ShoppingBasket className="h-20 w-20" />
+
 								<h3 className="font-semibold text-2xl">Your cart is empty</h3>
 								<p className="text-muted-foreground text-center">
 									Whoops! Nothing to show here yet.
