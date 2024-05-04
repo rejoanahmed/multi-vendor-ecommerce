8585bb42d5e8ba611f24b4538951b065d399e81a
YerzhanAbd <yerzhan.abd0@gmail.com>
2024-05-04 20:16:27 +0800

    refactor

--- src/app/trackorder/page.tsx
+++ src/app/trackorder/page.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Trackorder() {
-  // Basic styling for the message
-  const messageStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh', // Adjust this value as needed
-    fontSize: '20px', // Adjust font size as needed
-    color: '#333', // This is a generic color, feel free to change it
-    backgroundColor: '#f0f0f0', // Light grey background, feel free to change it
-    fontFamily: 'Arial, sans-serif' // A generic font family
-  }
+	const messageStyle = {
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		height: "100vh",
+		fontSize: "20px",
+		color: "#333",
+		backgroundColor: "#f0f0f0",
+		fontFamily: "Arial, sans-serif",
+	};
 
-  return <div style={messageStyle}>Your order is still being collected!</div>
+	return <div style={messageStyle}>Your order is still being collected!</div>;
 }
 
-export default Trackorder
+export default Trackorder;
